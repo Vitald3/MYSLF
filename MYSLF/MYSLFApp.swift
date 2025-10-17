0186ed2f94fd4b996cb8a011243cc371b512ae1a
vitald3 <vitald333@gmail.com>
2025-10-17 11:24:15 +0300

    Initial Commit

---
+++ MYSLF/MYSLFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYSLFApp.swift
+//  MYSLF
+//
+//  Created by Vitald3 on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MYSLFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
